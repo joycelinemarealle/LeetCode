ac2fcd0a2de0589ab396226f1d3c6ca1acc699e4
JOYCELINE MAREALLE <149646155+joycelinemarealle@users.noreply.github.com>
2024-11-24 09:34:48 -0500

    Create file

---
+++ 283-MoveZeroes/283-MoveZeroes.py
@@ -0,0 +1,6 @@
+ def moveZeroes(self, nums: List[int]) -> None:
+        i = 0
+        for j in range(len(nums)):
+            if nums[j] != 0:
+                nums[i], nums[j] = nums[j], nums[i]
+                i += 1
