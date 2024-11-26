54ecf83802ea0426eef330a19269dc53821bbf35
JOYCELINE MAREALLE <149646155+joycelinemarealle@users.noreply.github.com>
2024-11-25 19:44:16 -0500

    Create file

---
+++ 1456-MaximumNumberofVowelsinaSubstringofGivenLength/1456-MaximumNumberofVowelsinaSubstringofGivenLength.py
@@ -0,0 +1,8 @@
+        # Maximum vowels i.e. ans
+        ans: int = 0
+            
+        # Vowels in current window
+        currCount: int = 0
+            
+        # String of vowels
+        vowels: str = "aeiou"
